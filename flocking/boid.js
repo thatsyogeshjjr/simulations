b41e7e249cada55073b36e3523cf806c594e6bfb
Yogesh Jajoria <yogeshjajoria2019@gmail.com>
2024-11-06 08:03:25 +0530

    boid

--- flocking/boid.js
+++ flocking/boid.js
@@ -1,13 +1,18 @@
 class Boid {
   constructor() {
     this.position = createVector(width / 2, height / 2);
-    this.velocity = createVector();
+    this.velocity = p5.Vector.random2D();
     this.acceleration = createVector();
+  }
+
+  update() {
+    this.position.add(this.velocity);
+    this.velocity.add(this.acceleration);
   }
 
   show() {
     strokeWeight(16);
     stroke(255);
-    PointerEvent(this.position.x, this.position.y);
+    point(this.position.x, this.position.y);
   }
 }
