d5c37729040e29a5c7a0e30c6b6c05689b0a4a38
Yogesh Jajoria <yogeshjajoria2019@gmail.com>
2024-11-06 10:43:37 +0530

    alignment0.1

--- flocking/boid.js
+++ flocking/boid.js
@@ -1,8 +1,10 @@
 class Boid {
   constructor() {
-    this.position = createVector(width / 2, height / 2);
+    this.position = createVector(random(width), random(height));
     this.velocity = p5.Vector.random2D();
+    this.velocity.setMag(random(2, 4));
     this.acceleration = createVector();
+    this.maxForce = 1;
   }
 
   update() {
@@ -10,8 +12,39 @@ class Boid {
     this.velocity.add(this.acceleration);
   }
 
+  align(boids) {
+    let steering = createVector();
+    let total = 0;
+    let perception = 50;
+    for (let other of boids) {
+      let d = dist(
+        this.position.x,
+        this.position.y,
+        other.position.x,
+        other.position.y
+      );
+
+      if (other != this && d < perception) {
+        steering.add(other.velocity);
+        total++;
+      }
+    }
+    if (total > 0) {
+      steering.div(total);
+      steering.sub(this.velocity); // steering force = desired velocity - current velocity
+      steering.limit(this.maxForce);
+    }
+
+    return steering;
+  }
+
+  flock(boids) {
+    let alignment = this.align(boids);
+    this.acceleration = alignment; //  acceleration = force / mass (a unit mass is assumed)
+  }
+
   show() {
-    strokeWeight(16);
+    strokeWeight(8);
     stroke(255);
     point(this.position.x, this.position.y);
   }
--- flocking/sketch.js
+++ flocking/sketch.js
@@ -2,12 +2,15 @@ const flock = [];
 
 function setup() {
   createCanvas(640, 360);
-  flock.push(new Boid());
+  for (let i = 0; i < 100; i++) {
+    flock.push(new Boid());
+  }
 }
 
 function draw() {
   background(51);
   for (let boid of flock) {
+    boid.flock(flock);
     boid.update();
     boid.show();
   }
